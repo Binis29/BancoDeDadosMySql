6c15832e450283abad03765b9d4590ce12e3a500
binis29 <cleitonaguiarandrade@hotmail.com>
2020-09-22 12:31:02 -0300

    teste MySql 22/09/2020 12:30

--- Cleiton-aula22-09-DML.sql
+++ Cleiton-aula22-09-DML.sql
@@ -1,15 +1,14 @@
 create database db_deletar;
 use db_deletar;
 
-create table consumidores(
-id int PRIMARY KEY AUTO_INCREMENT,
-nome varchar(30)
+CREATE TABLE consumidores (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(30)
 );
 
 insert into consumidores (nome) values
-("Marcelo"),
-("Julia");
-
+	("Marcelo"),
+	("Julia");
 
 
 select * from consumidores;
